0c927b82a3501ed991fad8551a84e4eda7206d5e
Mike Munsie <mmunsie@researchnow.com>
2015-08-29 10:26:44 -0500

    Adding a structure for easy controllers, config, and shiz like that.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,18 @@
-angular.module('app.controllers', [])
+(function() {
 
-.controller('ListCtrl', function($scope) {
-	
-})
+  angular
+    .module('app.controllers', [])
+    .controller('ListController', ListController)
+    .controller("AnimationController", AnimationController);
+
+  function ListController() {
+
+  }
+
+  function AnimationController() {
+    var vm = this;
+    vm.name = "Mike";
+    alert("Cool")
+  }
+
+})();
